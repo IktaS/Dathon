1a17111188c23d876cf448d426ea79a8d023999d
seijaku <abdrohman0100@gmail.com>
2021-07-06 23:20:38 +0700

    merge conflict

--- client/client.py
+++ client/client.py
@@ -1,35 +1,50 @@
-#import library
+import socket
+import sys
 import pygame
+import os
+from menu import Button
+from menu import TextStatic
+from menu import TextButton
+from game_constant import *
 
-#initialize
-pygame.init()
-# screen = pygame.display.set_mode((1440, 1024))
-infoObject = pygame.display.Info()
-screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
-pygame.display.set_caption("Dathon")
 
-#define RGB value
-white = (255, 255, 255)
-black = (0, 0, 0)
 
-font = pygame.font.Font('assets/fonts/Poppins-Medium.ttf', 54)
-title = font.render('Dathon', True, black)
-title_dest = (620, 48)
+def main():
+    pygame.init()
+    title="Dathon"
+    screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+    pygame.display.set_caption(title)
+    clock = pygame.time.Clock()
+    screen.fill(CLR_Parchment)
+    # screen.fill(CLR_BG_MENU)
+    
+    # Menu
+    title_font=pygame.font.Font(os.path.join("assets","fonts",'Poppins-Bold.ttf'),88)
+    menu_font=pygame.font.Font(os.path.join("assets","fonts",'Poppins-Bold.ttf'),36)
+    menu=["Create Game","Join Game","How to Play?"]
+    text_title = TextStatic(screen,title_font,title,CLR_Paarl,553,128)
+    btn_cgame = Button(screen,menu_font,menu[0],CLR_Tan,CLR_ProvincialPink,CLR_Tan,CLR_Tan,MENU_BTN_W,MENU_BTN_H,SCREEN_W/2 - MENU_BTN_W/2,585,MENU_BTN_BORDER,MENU_BTN_EDGE)
+    btn_jgame = Button(screen,menu_font,menu[1],CLR_Tan,CLR_ProvincialPink,CLR_Tan,CLR_Tan,MENU_BTN_W,MENU_BTN_H,SCREEN_W/2 - MENU_BTN_W/2,726,MENU_BTN_BORDER,MENU_BTN_EDGE)
+    text_htp = TextButton(screen,menu_font,menu[2],CLR_Paarl,600,867)
+    is_running=True
+    
+    while is_running:
+        screen.fill(CLR_Parchment)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running=False
+            btn_cgame.event_handler(event)
+            btn_jgame.event_handler(event)
+            text_htp.event_handler(event)
+        pygame.draw.line(screen,CLR_TTL,(570,254),(874,254),1)   
+        btn_cgame.update()
+        btn_jgame.update()  
+        text_htp.update()
+        text_title.update()   
+        pygame.display.flip()
+        clock.tick()
+        # print(screen.get)
+    pygame.quit()
 
-dakon_board = pygame.image.load("assets/DakonBoard.png")
-dakon_board_dest = (42, 290)
-
-while True:
-    screen.fill(white)
-   
-    screen.blit(title, title_dest)
-    screen.blit(dakon_board, dakon_board_dest)
-
-    pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    pygame.display.update()
+if __name__ == '__main__':
+    main()
---
+++ client/game_constant.py
@@ -0,0 +1,41 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read("env")
+
+APP_HOST = config.get("app", "APP_HOST")
+APP_PORT = config.get("app", "APP_PORT")
+
+SCREEN_W = int(config.get("game", "WIDTH"))
+SCREEN_H = int(config.get("game", "HEIGHT"))
+
+# Color https://chir.ag/projects/name-that-color :)
+CLR_ProvincialPink = (254, 243, 243)
+CLR_Parchment = (238, 228, 208)
+CLR_Tan = (207, 166, 124)
+CLR_Paarl = (160, 93, 49)
+CLR_SpicyMix = (148, 91, 69)
+CLR_KenyanCopper = (109, 31, 9)
+CLR_Stonewall = (149, 137, 123)
+CLR_Black = (0,0,0)
+
+
+# MENU
+## Button
+MENU_BTN_BORDER=2
+MENU_BTN_EDGE=21
+MENU_BTN_W=606
+MENU_BTN_H=100
+MENU_INPUT_W=500
+
+
+CLR_BG_MENU=(238,228,208)
+CLR_DEFAULT_BUTTON= CLR_BG_MENU;
+CLR_ACTIVE_BUTTON= (207, 166, 124);
+CLR_ACTIVE_BUTTON_TEXT= (254, 243, 243);
+CLR_DEFAULT_BUTTON_TEXT= (207, 166, 124);
+CLR_DEFAULT_BUTTON_BORDER= (207, 166, 124);
+CLR_HTP= (160, 93, 49);
+CLR_TTL= (109, 31, 9,1);
+BLACK= (0, 0, 0,0.5);
+
