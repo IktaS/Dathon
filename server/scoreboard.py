7a6134b472c2868df82ddba5936a6c50075de36d
IktaS <imamrafiia@gmail.com>
2021-07-15 20:12:41 +0700

    add save

--- server/scoreboard.py
+++ server/scoreboard.py
@@ -2,11 +2,17 @@ import json
 
 
 class Scoreboard:
-    def __init__(self, jsonfile) -> None:
-        self.scores = json.load(jsonfile)
+    def __init__(self, file) -> None:
+        self.file = file
+        self.scores = json.load(file)
 
     def addScore(self, username, score):
         self.scores[username] = score
 
     def toJSON(self):
         return json.dumps(self.scores)
+
+    def save(self, data):
+        self.file.seek(0)
+        self.file.write(data)
+        self.file.truncate()
